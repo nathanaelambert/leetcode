e22582278142d7009e8e8ecb6c0fa7dbc8ad45ce
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-09-23 12:44:02 -0400

    0005: 285ms (51.64%) | 20.59MB (12.13%)

--- 0005.py
+++ 0005.py
@@ -7,9 +7,6 @@ def longestPalindrome(s: str) -> str:
     pals = [expand(i,i) for i in range(len(s))] + [expand(i,i+1) for i in range(len(s)-1) if s[i]==s[i+1]]
     return max(pals, key=len) if len(pals) > 0 else ''
 
-        
-
-
 if __name__ == "__main__":
     assert longestPalindrome("abbcccba") == "bcccb"
     assert longestPalindrome("cbbd") == "bb"
