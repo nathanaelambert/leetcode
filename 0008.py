da3bd1b0832f8195e13163801e3db9c0fec51614
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-09-23 12:35:33 -0400

    0008: 4ms 19.36MB

--- 0008.py
+++ 0008.py
@@ -1,16 +1,19 @@
-import re
-
-
 def myAtoi(s: str) -> int:
     s = s.lstrip()
+    if not s: return 0
     neg = s[0] == '-'
     s = s[1:] if s[0] == '-' or s[0] == '+' else s
     s.lstrip("0")
-    match = re.match(r'\d+', s)
-    number = int(match.group()) if match else 0
-    number = number*(-1) if neg else number
-    return min(2**31 -1, max(-2**31, number))
 
+    i = 0
+    number = 0
+    while i < len(s) and s[i].isdigit():
+        number += int(s[i])
+        number *= 10
+        i += 1
+
+    number = number // 10 *(-1) if neg else number // 10 
+    return min(2**31 -1, max(-2**31, number))
 
 
 
